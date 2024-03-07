ae053cde34221c1f071274d6c8f815f8f072f8f0
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-03-07 20:51:34 +0200

    Create All Expenses Header

---
+++ lib/widgets/all_expenses_widgets/all_expenses_header.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+
+class AllExpensesHeader extends StatelessWidget {
+  const AllExpensesHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'All Expenses',
+          style: AppStyles.styleSemiBold20(context),
+        ),
+        const Expanded(
+          child: SizedBox(),
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                width: 1,
+                color: Color(0xFFF1F1F1),
+              ),
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+          ),
+          child: Row(
+            children: [
+              Text(
+                'Monthly',
+                style: AppStyles.styleMedium16(context),
+              ),
+              const SizedBox(width: 8),
+              Transform.rotate(
+                angle: -1.57079633,
+                child: const Icon(
+                  Icons.arrow_back_ios_new_outlined,
+                  size: 15,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
