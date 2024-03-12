6271510b9d08532276f0445e98e8ff651075742a
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-03-12 14:24:21 +0200

    Income Section Done

--- lib/widgets/all_expenses_and_quick_invoice_section/all_expenses_and_quick_invoice_section.dart
+++ lib/widgets/all_expenses_and_quick_invoice_section/all_expenses_and_quick_invoice_section.dart
@@ -8,7 +8,7 @@ class AllExpensesAndQuickInvoiceSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const Padding(
-      padding: EdgeInsets.only(top:20),
+      padding: EdgeInsets.only(top:30),
       child: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'all_expenses_and_quick_invoice_section/all_expenses_and_quick_invoice_section.dart';
 import 'drawer_widgets/custom_drawer.dart';
 import 'my_cards_and_transaction_history_section/card_widgets/card_section.dart';
+import 'my_cards_and_transaction_history_section/income_section/income_section.dart';
 import 'my_cards_and_transaction_history_section/my_cards_and_transaction_history_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
@@ -22,7 +23,14 @@ class DashboardDesktopLayout extends StatelessWidget {
         SizedBox(width: 32),
         Expanded(
           flex: 2,
-          child:MyCardsAndTransactionHistorySection(),
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              MyCardsAndTransactionHistorySection(),
+              SizedBox(height: 24),
+              IncomeSection(),
+            ],
+          ),
         ),
       ],
     );
