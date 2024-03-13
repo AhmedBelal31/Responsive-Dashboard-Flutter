3c4b365ae40bcd2f4f558e0641a20c59aa40b016
Ahmedbelal31 <ahmedbelal1.exe@gmail.com>
2024-03-13 22:20:15 +0200

    Implement Mobile Layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/utils/widgets/adaptive_layout_widget.dart';
-
+import '../utils/size_config.dart';
+import '../utils/widgets/adaptive_layout_widget.dart';
 import '../widgets/dashboard_desktop_layout.dart';
+import '../widgets/dashboard_mobile_layout.dart';
+import '../widgets/dashboard_tablet_layout.dart';
+import '../widgets/drawer_widgets/custom_drawer.dart';
 
-class DashBoardView extends StatelessWidget {
+class DashBoardView extends StatefulWidget {
   const DashBoardView({super.key});
 
   @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
     return Scaffold(
-      backgroundColor:const Color(0xFFF7F9FA),
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? AppBar(
+        elevation: 0,
+        backgroundColor: const Color(0xFFFAFAFA),
+        scrolledUnderElevation: 0,
+        leading: IconButton(
+            onPressed: () {
+              scaffoldKey.currentState!.openDrawer();
+            },
+            icon: const Icon(Icons.menu)),
+      )
+          : null,
+      backgroundColor: const Color(0xFFF7F9FA),
+      drawer: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? const CustomDrawer()
+          : null,
       body: AdaptiveLayout(
-        mobileLayout: (context) => const SizedBox(),
-        tabletLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashBoardMobileLayout(),
+        tabletLayout: (context) => const DashBoardTabletLayout(),
         desktopLayout: (context) => const DashboardDesktopLayout(),
       ),
     );
